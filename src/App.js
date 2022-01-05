33236420bce106761253e2d127173d2fb1610c2b
Veeresh R G <veeresh.ravi@gmail.com>
2022-01-06 01:18:32 +0530

    Front End

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Dashboard from './components/IndiDashboard';
+import TeamDashboard from './components/TeamDashboard';
+import Project from './components/Project';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router className='m-0 p-0'>
+      <div className="App bg-slate-300">
+        <div className='content'>
+
+          <Switch>
+
+            <Route exact path="/">
+              <TeamDashboard />
+            </Route>
+            <Route exact path="/project">
+              <Project />
+            </Route>
+            <Route exact path="/indi">
+              <Dashboard />
+            </Route>
+
+          </Switch>
+
+        </div>
+
+
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+// <div className="App bg-slate-300">
+//   <TeamDashboard />
+// </div>
