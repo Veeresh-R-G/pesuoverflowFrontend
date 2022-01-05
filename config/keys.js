c9c56a77e93084c8304d52e073e63f3f21b23f41
Veeresh R G <72454785+Veeresh-R-G@users.noreply.github.com>
2022-01-05 21:14:04 +0530

    Updated keys.js
    removed personal credentials

--- config/keys.js
+++ config/keys.js
@@ -3,10 +3,10 @@
 module.exports = {
     google: {
         callbackURL: "/auth/google/redirect",
-        clientID: '735679627577-htr86h2khvkq0428od0frcoode4ofkff.apps.googleusercontent.com',
-        clientSecret: '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'
+        clientID: '<Your Google Client ID>',
+        clientSecret: '< Your Google Client Secret - key >'
     },
     session: {
-        cookieKey: 'thenetninjaisawesomeiguess'
+        cookieKey: 'anythinghere--> youareawesome'
     }
 };
